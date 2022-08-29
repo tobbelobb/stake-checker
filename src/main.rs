62262994392827f68f34eb0b927747a908214415
Torbjørn Ludvigsen <tobben@fastmail.fm>
2022-08-29 11:04:38 +0200

    Minimized example
    Put it on playground:
    https://play.rust-lang.org/?version=nightly&mode=debug&edition=2021&gist=2efc38c82d9d4b8ae0297447bbec9acf
    Filed an issue:
    https://github.com/rust-lang/rust/issues/101149

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,16 @@
-use std::fmt;
-use util::rpc;
-
-use clap::Command;
-use log::error;
-use log4rs;
-
-#[derive(Debug)]
-enum OneError {
-    NoEndpoint,
-    NoPolkadotAddr,
-    CSV(csv::Error),
-    IO(std::io::Error),
-    Reqwest(reqwest::Error),
+async fn another_async() -> bool {
+    async { true }.await
 }
 
-impl std::error::Error for OneError {}
-
-impl fmt::Display for OneError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            OneError::NoEndpoint => write!(f, "No RPC endpoint is set via the .env variable."),
-            OneError::NoPolkadotAddr => {
-                write!(f, "No Polkadot address is set via the .env variable.")
-            }
-            OneError::CSV(err) => write!(f, "Error while writing the CSV file {}", err),
-            OneError::IO(err) => write!(f, "Error while flushing the file {}", err),
-            OneError::Reqwest(err) => write!(f, "Error while fetching data {}", err),
-        }
-    }
-}
-
-impl From<reqwest::Error> for OneError {
-    fn from(err: reqwest::Error) -> OneError {
-        OneError::Reqwest(err)
-    }
-}
-
-impl From<csv::Error> for OneError {
-    fn from(err: csv::Error) -> OneError {
-        OneError::CSV(err)
-    }
-}
-
-impl From<std::io::Error> for OneError {
-    fn from(err: std::io::Error) -> OneError {
-        OneError::IO(err)
-    }
-}
-
-async fn rpc_methods(rpc_endpoint: &str) -> Result<(), OneError> {
-    let ans = rpc(rpc_endpoint, "rpc_methods", ()).await?;
-    println!("{}", serde_json::to_string_pretty(&ans).unwrap());
-    Ok(())
+async fn one_async() -> bool {
+    another_async().await
 }
 
 #[tokio::main]
-async fn main() -> Result<(), OneError> {
-    dotenv::dotenv().ok();
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
-
-    let _matches = Command::new("Stake Checker")
-        .version("1.0")
-        .author("Torbjørn L. <tobben@fastmail.fm>")
-        .about("Get Polkadot Staking Rewards");
-
-    let rpc_endpoint = dotenv::var("RPC_ENDPOINT").expect("RPC endpoint not set");
-    let polkadot_addr = dotenv::var("POLKADOT_ADDR").expect("Polkadot address not set");
-    if rpc_endpoint.is_empty() {
-        error!("Empty RPC endpoint provided! Please set one via the .env file!");
-        return Err(OneError::NoEndpoint);
-    }
-    if polkadot_addr.is_empty() {
-        error!("Empty Polkadot endpoint provided! Please set one via the .env file!");
-        return Err(OneError::NoPolkadotAddr);
+async fn main() {
+    if false {
+        return Err(());
     }
 
-    rpc_methods(&rpc_endpoint)
+    one_async()
 }
